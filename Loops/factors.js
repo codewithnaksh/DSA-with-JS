dd56a15484778ae0d5a0050c53262823be221d5e
codewithnaksh <nakshatra.2214502138@mujonline.edu.in>
2025-04-20 15:55:19 +0530

    add factors of a number

---
+++ Loops/factors.js
@@ -0,0 +1,22 @@
+// Define a number for which the factors will be calculated
+let num = 15;
+
+// Function to find and print all factors of a given number
+function factors(n) {
+  console.log("Factors of", n, "are ->"); // Print the header message
+
+  console.log(1); // 1 is always a factor of any number
+
+  // Loop from 2 to n/2 to find factors
+  for (let i = 2; i <= n / 2; i++) {
+    if (n % i == 0) {
+      // Check if i is a factor of n
+      console.log(i); // Print the factor
+    }
+  }
+
+  console.log(n); // The number itself is always a factor
+}
+
+// Call the function to print factors of the given number
+factors(num);
